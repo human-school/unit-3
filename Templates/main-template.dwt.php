726049101d70f909c47434ca0607e70dccd178da
this-is-for-a-school-thing <matthew.j.cummings123@gmail.com>
2022-04-01 13:29:02 +1100

    start

---
+++ Templates/main-template.dwt.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tbody>
+    <tr>
+      <td width="7%" height="100"><img src="../images/Logo-main.png" width="95" height="89" alt=""/></td>
+      <td width="93%">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="1000">&nbsp;</td>
+      <td align="left" valign="top"><table width="100%" height="495%" border="1">
+        <tbody>
+          <tr>
+            <td width="64%" height="2261" align="left" valign="top"><!-- TemplateBeginEditable name="main" -->main<!-- TemplateEndEditable --></td>
+            <td width="36%" align="left" valign="top"><table width="100%" border="1">
+              <tbody>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td height="333.333">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td height="333.333">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/main-template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tbody>
+    <tr>
+      <td width="7%" height="100"><img src="images/Logo-main.png" width="95" height="89" alt=""/></td>
+      <td width="93%">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="1000">&nbsp;</td>
+      <td align="left" valign="top"><table width="100%" height="495%" border="1">
+        <tbody>
+          <tr>
+            <td width="64%" height="2261" align="left" valign="top"><!-- InstanceBeginEditable name="main" -->main<!-- InstanceEndEditable --></td>
+            <td width="36%" align="left" valign="top"><table width="100%" border="1">
+              <tbody>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td height="333.333">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td height="333.333">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td height="333.333">&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+              </tbody>
+            </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+<!-- InstanceEnd --></html>
