0c0313450345a709f0bf32d93b38782ac9b51bf2
this-is-for-a-school-thing <matthew.j.cummings123@gmail.com>
2022-04-29 13:14:51 +1000

    logo
    E

--- Templates/main-template.dwt.php
+++ Templates/main-template.dwt.php
@@ -6,18 +6,20 @@
 <title>Untitled Document</title>
 <!-- TemplateEndEditable -->
 <!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
+<!-- TemplateEndEditable -->,m 
 </head>
 
 <body>
 <table width="100%" border="1">
   <tbody>
     <tr>
-      <td width="7%" height="100"><img src="../images/Logo-main.png" width="95" height="89" alt=""/></td>
+      <td width="7%" height="100"><a href="../index.php"><img src="../images/Logo-main.png" alt="RFH" width="95" height="89" title="RFH"/></a></td>
       <td width="93%">&nbsp;</td>
     </tr>
     <tr>
-      <td height="1000">&nbsp;</td>
+      <td height="1000" align="left" valign="top"><p><a href="../index.php"><img src="../images/Home (3).png" alt="home" width="100" height="50" title="home"/></a></p>
+      <p><a href="../podcast.php"><img src="../images/Podcast.png" alt="podcast" width="100" height="50" title="podcast"/></a></p>
+      <p><a href="../info.php"><img src="../images/info.png" alt="info" width="100" height="50" title="info"/></a></p></td>
       <td align="left" valign="top"><table width="100%" height="495%" border="1">
         <tbody>
           <tr>
@@ -25,8 +27,9 @@
             <td width="36%" align="left" valign="top"><table width="100%" border="1">
               <tbody>
                 <tr>
-                  <td height="333.333">&nbsp;</td>
-                  <td height="333.333">&nbsp;</td>
+                  <td width="50%" height="333.333" align="left" valign="top"><p><a href="https://deepfriedmemes.com/"><img src="../images/ist.jpg" alt="" width="176" height="262" title="deep fry your images by clicking here"/></a></p>
+                    <p>deep fry your images by clicking above</p></td>
+                  <td width="50%" height="333.333">&nbsp;</td>
                 </tr>
                 <tr>
                   <td height="333.333">&nbsp;</td>
