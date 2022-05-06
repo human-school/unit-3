a50672b88814ae90250db763295f0b308713629b
this-is-for-a-school-thing <matthew.j.cummings123@gmail.com>
2022-05-06 13:06:31 +1000

    end of week
    6.5.22

--- info.php
+++ info.php
@@ -6,41 +6,25 @@
 <title>Untitled Document</title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
+<!-- InstanceEndEditable -->,m 
 </head>
-
-<body>
+<body bgcolor = black>
 <table width="100%" border="1">
   <tbody>
     <tr>
       <td width="7%" height="100"><a href="index.php"><img src="images/Logo-main.png" alt="RFH" width="95" height="89" title="RFH"/></a></td>
-      <td width="93%">&nbsp;</td>
+      <td width="93%" align="center"><a href="https://github.com/this-is-for-a-school-thing/unit-3"><img src="images/radio%20for%20homosapiens.png" alt="github" width="1123" height="94" title="github"/></a></td>
     </tr>
     <tr>
       <td height="1000" align="left" valign="top"><p><a href="index.php"><img src="images/Home%20(3).png" alt="home" width="100" height="50" title="home"/></a></p>
       <p><a href="podcast.php"><img src="images/Podcast.png" alt="podcast" width="100" height="50" title="podcast"/></a></p>
-      <p><a href="info.php"><img src="images/info.png" alt="info" width="100" height="50" title="info"/></a></p></td>
+      <p><a href="info.php"><img src="images/info.png" alt="info" width="100" height="50" title="info"/></a></p>
+      <p>&nbsp;</p></td>
       <td align="left" valign="top"><table width="100%" height="495%" border="1">
         <tbody>
           <tr>
-            <td width="64%" height="2261" align="left" valign="top"><!-- InstanceBeginEditable name="main" -->main<!-- InstanceEndEditable --></td>
-            <td width="36%" align="left" valign="top"><table width="100%" border="1">
-              <tbody>
-                <tr>
-                  <td width="50%" height="333.333" align="left" valign="top"><p><a href="https://deepfriedmemes.com/"><img src="images/ist.jpg" alt="" width="176" height="262" title="deep fry your images by clicking here"/></a></p>
-                    <p>deep fry your images by clicking above</p></td>
-                  <td width="50%" height="333.333">&nbsp;</td>
-                </tr>
-                <tr>
-                  <td height="333.333">&nbsp;</td>
-                  <td height="333.333">&nbsp;</td>
-                </tr>
-                <tr>
-                  <td height="333.333">&nbsp;</td>
-                  <td>&nbsp;</td>
-                </tr>
-              </tbody>
-            </table></td>
+            <td width="64%" height="2261" align="left" valign="top" style="color: #FFFFFF">thing</td>
+            <td width="36%" align="left" valign="top"><a href="https://deepfriedmemes.com/"><img src="images/ist.jpg" alt="ad" width="518" height="225" title="ad"style="color: #FFFFFF"/> click above to deep fry you images </td>
           </tr>
         </tbody>
       </table></td>
