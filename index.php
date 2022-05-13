dccaaa9e571853e44b148176d36fe170b1ddfa27
this-is-for-a-school-thing <matthew.j.cummings123@gmail.com>
2022-05-13 13:27:39 +1000

    end of week 13.5
    A

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <!doctype html>
-<html><!-- InstanceBegin template="/Templates/main-template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html><!-- InstanceBegin template="/Templates/main_replace.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="utf-8">
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Untitled Document</title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->,m 
+<!-- InstanceEndEditable -->
 </head>
+
 <body bgcolor = black>
 <table width="100%" border="1">
   <tbody>
     <tr>
-      <td width="7%" height="100"><a href="index.php"><img src="images/Logo-main.png" alt="RFH" width="95" height="89" title="RFH"/></a></td>
-      <td width="93%" align="center"><a href="https://github.com/this-is-for-a-school-thing/unit-3"><img src="images/radio%20for%20homosapiens.png" alt="github" width="1123" height="94" title="github"/></a></td>
+      <td width="7%" height="100"><a href="index.php"><img src="images/Logo-main.png" alt="RFH" width="95" height="89" title="RFH_home_button"/></a></td>
+      <td width="93%"><img src="images/radio for homosapiens.png" width="1537" height="100" alt=""/></td>
     </tr>
     <tr>
-      <td height="1000" align="left" valign="top"><p><a href="index.php"><img src="images/Home%20(3).png" alt="home" width="100" height="50" title="home"/></a></p>
+      <td height="1000" align="left" valign="top"><p><a href="index.php"><img src="images/Home (3).png" alt="home" width="100" height="50" title="home"/></a></p>
       <p><a href="podcast.php"><img src="images/Podcast.png" alt="podcast" width="100" height="50" title="podcast"/></a></p>
-      <p><a href="info.php"><img src="images/info.png" alt="info" width="100" height="50" title="info"/></a></p>
-      <p>&nbsp;</p></td>
+      <p><a href="info.php"><img src="images/info.png" alt="info" width="100" height="50" title="info"/></a></p></td>
       <td align="left" valign="top"><table width="100%" height="495%" border="1">
         <tbody>
           <tr>
-            <td width="64%" height="2261" align="left" valign="top" style="color: #FFFFFF">thing</td>
-            <td width="36%" align="left" valign="top"><a href="https://deepfriedmemes.com/"><img src="images/ist.jpg" alt="ad" width="518" height="225" title="ad"style="color: #FFFFFF"/> click above to deep fry you images </td>
-          </tr>
-        </tbody>
-      </table></td>
-    </tr>
-  </tbody>
+            <td width="64%" height="2261" align="left" valign="top" style="color: #FFFFFF" ><!-- InstanceBeginEditable name="main" -->index<!-- InstanceEndEditable -->
+              <table width="25%" height="1212" border="1" align="right">
+                <tbody>
+                  <tr>
+                    <td align="left" valign="top" style="color: #FFFFFF"><p><a href="https://deepfriedmemes.com/"><img src="images/ist.jpg" alt="ad_deepfry" width="405" height="302" title="ad_deepfry"/></a></p>
+                    <p><a href="https://deepfriedmemes.com/">click above to deep fry you images</a></p></td>
+                  </tr>
+                </tbody>
+            </table></td>
+    </tbody>       
 </table>
 </body>
 <!-- InstanceEnd --></html>
